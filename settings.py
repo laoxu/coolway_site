d5dce2d2f33285e9b49a9dbac98b41bb486091a8
laoxu <zhaopeng.xuzp@gmail.com>
2012-10-29 15:54:39 +0800

    fix bug
    fix bug

--- settings.py
+++ settings.py
@@ -101,7 +101,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'coolway.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
