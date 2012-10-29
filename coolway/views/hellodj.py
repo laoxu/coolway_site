4cdef1d057a31662fcd33571bd79abf46f046f54
laoxu <zhaopeng.xuzp@gmail.com>
2012-10-29 18:20:58 +0800

    delete code
    delete code

--- coolway/views/hellodj.py
+++ coolway/views/hellodj.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 import datetime
 
-
 def index(request):
     now = datetime.datetime.now()
     return render_to_response('index.html', {'current_date': now})
