66e7c17907787bb9378bf4c048021d28ffba952a
51weekend <51weekend@gmail.com>
2012-11-04 14:55:19 +0800

    把部分配置分离出来、在这里配置db,邮件服务器，服务本身相关配置

--- settings_local.py
+++ settings_local.py
@@ -1,23 +1,20 @@
+# -*- encoding: utf-8 -*-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'coolway',                      # Or path to database file if using sqlite3.
-        # 'USER': 'root',                      # Not used with sqlite3.
-        # 'PASSWORD': 'cdggcdgg',                  # Not used with sqlite3.
-        # 'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.    
+        'USER': 'coolway',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.    
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
-        'USER': 'admin',                      # Not used with sqlite3.
-        'PASSWORD': 'sfvxvi32$asv',                  # Not used with sqlite3.
-        'HOST': '42.121.89.124', 
     }
 }
-
-DEFAULT_FROM_EMAIL = "noreply@coolway.com"
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
 EMAIL_HOST='smtp.gmail.com'
+#先通过gmail发送邮件、下面设置自己的账号密码
 EMAIL_HOST_USER='51weekend@gmail.com'
-EMAIL_HOST_PASSWORD='cdggcdgg!82'
+EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
