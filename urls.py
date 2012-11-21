bd042c1009df56bce20218334a051002fcb402f1
51weekend <51weekend@gmail.com>
2012-11-21 08:51:47 +0800

    使用登陆、注册界面

--- urls.py
+++ urls.py
@@ -10,7 +10,9 @@ urlpatterns = patterns('',
     url(r'^$', index,name='index'),
     url(r'^accounts/',include('coolway_website.accounts.urls')),
     url(r'^post/',include('coolway_website.post.urls')),
-    url(r'^images/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_PATH})
+    url(r'^images/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_PATH}),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_URL})
+
 )
 #handler404 = 'coolway.views.meta.page'
 #handler500 = 'coolway.views.meta.error_handler'
