e6082ae0037bac7e0dddc28887eef2a76ee887b0
cheng dong <51weekend@gmail.com>
2012-11-26 15:41:47 +0800

    异步方式上传文件

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from coolway_website.views import *
+import os
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -10,8 +11,9 @@ urlpatterns = patterns('',
     url(r'^$', index,name='index'),
     url(r'^accounts/',include('coolway_website.accounts.urls')),
     url(r'^post/',include('coolway_website.post.urls')),
-    url(r'^images/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_PATH}),
-    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_URL})
+    (r'^upload/', include('coolway_website.fileupload.urls')),
+    url(r'^images/(?P<path>.*)$','django.views.static.serve',{'document_root':os.path.join(os.path.abspath(os.path.dirname(__file__)), 'images')}),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':os.path.join(os.path.abspath(os.path.dirname(__file__)), 'media')}),
 
 )
 #handler404 = 'coolway.views.meta.page'
